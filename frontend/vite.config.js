3b8c35da67bcedc1b844f2191a6f56cedf54d9ec
Franco Martinez <400651@sistemas.frc.utn.edu.ar>
2026-03-08 01:49:17 -0300

    correccion vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: './', // usar rutas relativas para despliegues en GitHub Pages
+  base: '/',
   plugins: [react()],
   server: {
     port: 5173,
